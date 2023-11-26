9eaa3c522af1acf8186382c53ce050635e7862bb
Carlos Roberto <crpu4a@gmail.com>
2023-11-26 11:00:49 -0300

    testes do livro

---
+++ ContactsList/AnotherClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsList
+{
+    internal class AnotherClass
+    {
+        public static void TestMain()
+        {
+            MessageBox.Show("Pow!");
+        }
+    }
+}
