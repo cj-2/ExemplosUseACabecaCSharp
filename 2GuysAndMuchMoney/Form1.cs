f5bb33793528d0bdbeb632048d756a91d0e5cc6d
Carlos Roberto <crpu4a@gmail.com>
2023-11-30 06:12:54 -0300

    Add project files.

---
+++ 2GuysAndMuchMoney/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2GuysAndMuchMoney
+{
+    public partial class Form1 : Form
+    {
+        Guy Joe;
+        Guy Bob;
+        int Bank = 100;
+
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = Joe.Name + " has R$ " + Joe.Cash;
+            bobsCashLabel.Text = Bob.Name + " has R$ " + Bob.Cash;
+            bankCashLabel.Text = "The Bank has R$ " + Bank;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Joe = new Guy();
+            Joe.Name = "Joe";
+            Joe.Cash = 50;
+
+            Bob = new Guy();
+            Bob.Name = "Bob";
+            Bob.Cash = 100;
+
+            UpdateForm();
+        }
+
+        private void GiveToJoeButton_Click(object sender, EventArgs e)
+        {
+            if (Bank >= 10)
+            {
+                Bank -= Joe.ReciveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The Bank is out of money.");
+            }
+        }
+
+        private void ReciveFromBobButton_Click(object sender, EventArgs e)
+        {
+            Bank += Bob.GiveCash(5);
+            UpdateForm();
+        }
+    }
+}
---
+++ 2GuysAndMuchMoney/Guy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2GuysAndMuchMoney
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if (Cash >= amount && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I don't have enough cash to give you " + amount, Name + " says ");
+                return 0;
+            }
+        }
+        public int ReciveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " isn't an amount I'll take", Name + " says...");
+                return 0;
+            }
+        }
+    }
+}
