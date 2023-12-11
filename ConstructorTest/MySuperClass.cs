060887f3ce2a36d95c48408a6fa05874cd24c8bd
Carlos Roberto <crpu4a@gmail.com>
2023-12-11 06:38:02 -0300

    Add project files.

---
+++ ConstructorTest/MySuperClass.cs
@@ -0,0 +1,16 @@
+﻿namespace ConstructorTest
+{
+    internal class MySuperClass
+    {
+        public MySuperClass(string superClassNeeds) {
+            Console.WriteLine("This is the superclass: " + superClassNeeds);
+        }
+    }
+
+    internal class MySubClass : MySuperClass {
+        public MySubClass(string superClassNeeds, int anotherValue) : base(superClassNeeds)
+        {
+            Console.WriteLine("This is the subclass: " + superClassNeeds + " and " + anotherValue);
+        }
+    }
+}
