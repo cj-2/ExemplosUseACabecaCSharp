8fcf6d38257e7e300c276d62a66f6283011f2917
Carlos Roberto <crpu4a@gmail.com>
2024-01-04 08:02:58 -0300

    Add project files.

---
+++ DuckConsole/Duck.cs
@@ -0,0 +1,14 @@
+﻿namespace DuckConsole
+{
+    internal class Duck
+    {
+        public int Size;
+
+        public KindOfDuck Kind;
+
+        public override string ToString()
+        {
+            return "Type: " + Kind.ToString() + ", Size: " + Size;
+        }
+    }
+}
---
+++ DuckConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using DuckConsole;
+
+List<Duck> Ducks = new List<Duck>() {
+    new Duck(){ Kind = KindOfDuck.Mallard, Size = 17 }, // sintaxe completa
+    new (){ Kind = KindOfDuck.Muscovy, Size = 18 }, // sintaxe reduzida
+    new (){ Kind = KindOfDuck.Decoy, Size = 14 },
+    new (){ Kind = KindOfDuck.Muscovy, Size = 11 },
+    new (){ Kind = KindOfDuck.Mallard, Size = 14 },
+    new (){ Kind = KindOfDuck.Decoy, Size = 13 },
+};
+
+foreach (Duck duck in Ducks)
+{
+    Console.WriteLine(duck.ToString());
+}
+
+Console.ReadKey();
