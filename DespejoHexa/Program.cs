8169f241f41a0ffd6b9abd426d6e65c75f3df15e
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2025-12-29 10:11:55 -0300

    .

---
+++ DespejoHexa/Program.cs
@@ -0,0 +1,46 @@
+﻿// Funciona apenas com arquivos de texto
+
+if (!File.Exists("inputFile.txt"))
+{
+    using var inputWriter = new StreamWriter("inputFile.txt", false);
+    inputWriter.Write("Esse é apenas um exemplo de texto que será exibido no console como hexadecimal.");
+}
+
+using var reader = new StreamReader("inputFile.txt");
+using var writer = new StreamWriter("outputFile.txt", false);
+
+var position = 0;
+
+while (!reader.EndOfStream)
+{
+    var buffer = new char[16];
+    var charactersRead = reader.ReadBlock(buffer, 0, 16);
+    
+    writer.Write("{0:x4}: ", position);
+    position += charactersRead;
+
+    for (var i = 0; i < 16; i++)
+    {
+        if (i < charactersRead)
+        {
+            writer.Write("{0:x2} ", (byte)buffer[i]);
+        }
+        else
+        {
+            writer.Write(" ");
+        }
+
+        if (i == 7)
+        {
+            writer.Write("-- ");
+        }
+
+        if (buffer[i] < 32 || buffer[i] > 250)
+        {
+            buffer[i] = '.';
+        }
+    }
+    
+    var bufferContents = new string(buffer);
+    writer.WriteLine(string.Concat(" ", bufferContents.AsSpan(0, charactersRead)));
+}
--- Testes/Program.cs
+++ Testes/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Exemplo de escita de números em hexa
+const int j = 0x20; // O 0x que informa que vamos digitar um número em hexa
+Console.WriteLine("Valor em decimal, pois é convertido em decimal: " + j);
+
+var h = String.Format("Já esse valor é em hexa: {0:x2}", j); // o x2 matem em hexa
+Console.WriteLine(h); // Aqui manteve no hexa
