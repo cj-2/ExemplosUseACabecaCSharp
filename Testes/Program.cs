97aaf55214c657f43a432cc0767b650a5b6796a8
Carlos Júnior <carlos.roberto.98jr@gmail.com>
2025-12-30 12:45:47 -0300

    exemplo de leitura e escrita de binários

--- Testes/Program.cs
+++ Testes/Program.cs
@@ -1,6 +1,26 @@
-﻿// Exemplo de escita de números em hexa
-const int j = 0x20; // O 0x que informa que vamos digitar um número em hexa
+﻿using System.Text;
+
+// Exemplos de impressão de hexadecimal
+const int j = 0x20; // O 0x que informa que vamos digitar um número em hexadecimal
 Console.WriteLine("Valor em decimal, pois é convertido em decimal: " + j);
 
-var h = String.Format("Já esse valor é em hexa: {0:x2}", j); // o x2 matem em hexa
-Console.WriteLine(h); // Aqui manteve no hexa
+var h = String.Format("Já esse valor é em hexa: {0:x2}", j); // o x2 mantem em hexadecimal
+Console.WriteLine(h);
+
+// Exemplo de leitura e escria de binários
+using (var file = File.Open("inputFile.bin", FileMode.OpenOrCreate))
+{
+    using var inputWriter = new BinaryWriter(file, Encoding.UTF8);
+    inputWriter.Write("Essa string será convertida em binário.");
+    inputWriter.Write(726381723);
+    inputWriter.Write("Outra string...");
+}
+
+using (var readFile = File.Open("inputFile.bin", FileMode.Open))
+{
+    using var fileText = new BinaryReader(readFile);
+    Console.WriteLine(fileText.ReadString());
+    Console.WriteLine(fileText.ReadInt32());
+    Console.WriteLine(fileText.ReadString());
+}
+
